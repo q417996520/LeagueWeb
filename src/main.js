e0eb98949fe04d6fa8490386617df5b2ac1040a6
q417996520 <417996520@qq.com>
2020-04-22 15:49:01 +0800

    第二次提交 reponded with status of  404 user/login

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+import Cookies from 'js-cookie'
+import store from './store'
+import Element from 'element-ui'
+import './styles/element-variables.scss'
 
 Vue.config.productionTip = false
 
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
